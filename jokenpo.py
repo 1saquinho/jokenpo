9689f86f4cdfb6bdec5c3bcc80612bcefd22217d
Isaque Correia <isaque.imb@gmail.com>
2025-05-19 19:45:12 -0300

    adicionado um comentário

--- jokenpo.py
+++ jokenpo.py
@@ -5,18 +5,20 @@ mao_jogador = None
 mao_pc = random.choice(jogada)
 
 while mao_jogador not in jogada:
-  mao_jogador = input("Escolha pedra, papel ou tesoura: ")
+    mao_jogador = input("Escolha pedra, papel ou tesoura: ")
 
 print(f"Jogador: {mao_jogador}")
 print(f"Computador: {mao_pc}")
 
 if mao_jogador == "pedra" and mao_pc == "tesoura":
-  print("Você ganhou! Pedra ganha de tesoura.")
+    print("Você ganhou! Pedra ganha de tesoura.")
 elif mao_jogador == "papel" and mao_pc == "pedra":
-  print("Você ganhou! Papel ganha de pedra.")
+    print("Você ganhou! Papel ganha de pedra.")
 elif mao_jogador == "tesoura" and mao_pc == "papel":
-  print("Você ganho! Tesoura ganha de papel.")
+    print("Você ganho! Tesoura ganha de papel.")
 elif mao_jogador == mao_pc:
-  print("Empate!")
+    print("Empate!")
 else:
-  print("Você perdeu! :( ")
+    print("Você perdeu! :( ")
+
+# comentário
