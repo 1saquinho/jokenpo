dcf42a312ea0ab991b7bd18cd72531d3ddef0054
1saquinho <126513295+1saquinho@users.noreply.github.com>
2025-05-19 18:54:22 -0300

    Add files via upload

---
+++ jokenpo.py
@@ -0,0 +1,22 @@
+import random
+
+jogada = ("pedra", "papel", "tesoura")
+mao_jogador = None
+mao_pc = random.choice(jogada)
+
+while mao_jogador not in jogada:
+  mao_jogador = input("Escolha pedra, papel ou tesoura: ")
+
+print(f"Jogador: {mao_jogador}")
+print(f"Computador: {mao_pc}")
+
+if mao_jogador == "pedra" and mao_pc == "tesoura":
+  print("Você ganhou! Pedra ganha de tesoura.")
+elif mao_jogador == "papel" and mao_pc == "pedra":
+  print("Você ganhou! Papel ganha de pedra.")
+elif mao_jogador == "tesoura" and mao_pc == "papel":
+  print("Você ganho! Tesoura ganha de papel.")
+elif mao_jogador == mao_pc:
+  print("Empate!")
+else:
+  print("Você perdeu! :( ")
